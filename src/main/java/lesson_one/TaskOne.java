5b6072439b2c7e657313d9905e98b570e42af75e
Alexandra <alexakanalash@gmail.com>
2026-05-12 21:04:15 +0200

    Intial commit

---
+++ src/main/java/lesson_one/TaskOne.java
@@ -0,0 +1,13 @@
+package lesson_one;
+
+public class TaskOne {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 4;
+        boolean isGreater = a > b;
+        System.out.println(isGreater);
+        boolean isEven = b % 2 == 0;
+        System.out.println(isEven);
+
+    }
+}
---
+++ src/main/java/lesson_one/TaskTwo.java
@@ -0,0 +1,8 @@
+package lesson_one;
+
+public class TaskTwo {
+    public static void main(String[] args) {
+        String name = "It's fine!";
+
+    }
+}
