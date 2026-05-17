3f13f71d03617343e9525c5e7268a6aab0e9d404
Alexandra <alexakanalash@gmail.com>
2026-05-17 14:35:46 +0200

    Tickets

---
+++ src/main/java/lesson_two/TaskFive.java
@@ -0,0 +1,36 @@
+package lesson_two;
+
+public class TaskFive {
+    public static void main(String[] args) {
+        int age = 44;
+        String zone = "обычная";
+        String timeOfDay = "утро";
+        if (age < 12) {
+            if (zone.equals("VIP")) {
+                System.out.println("Детский VIP-билет");
+            } else {
+                System.out.println("Детский билет");
+            }
+        } else if (age >= 12 & age <= 60) {
+            if (zone.equals("VIP")) {
+                if (timeOfDay.equals("вечер")) {
+                    System.out.println("Вечерний VIP-билет");
+                } else {
+                    System.out.println("Обычный VIP-билет");
+                }
+            } else if (zone.equals("обычная")) {
+                if (timeOfDay.equals("утро")) {
+                    System.out.println("Утренний билет");
+                } else {
+                    System.out.println("Стандарный билет");
+                }
+            }
+        } else {
+            if (zone.equals("VIP")) {
+                System.out.println("Пенсионный VIP-билет");
+            } else {
+                System.out.println("Пенсионный билет");
+            }
+        }
+    }
+}
